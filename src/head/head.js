7694bf1df5020ebcc116de65b280ca154d6583b7
cy19890513 <ychen012@qq.com>
2017-07-23 18:20:02 -0700

    add board game and head

---
+++ src/head/head.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './../index.css';
+
+
+
+class Head extends React.Component{
+  render(){
+    return (
+      <div>
+        Hello World!
+      </div>
+    );
+  }
+}
+
+export default Head
